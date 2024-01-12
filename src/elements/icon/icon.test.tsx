b63fae15dff0d47ad462327833e91a3e15fb1bd6
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-11 23:36:04 -0300

    feat: add icon element

---
+++ src/elements/icon/icon.test.tsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { Icon } from ".";
+
+describe('icon', () => {
+  it('should render icon component with style solid and icon star', () => {
+    const { container } = render(<Icon icon="star" />);
+    
+    expect(container.getElementsByClassName('fa-solid')).toBeDefined();
+    expect(container.getElementsByClassName('fa-star')).toBeDefined();
+  })
+
+  it('should render icon component with style regular and icon star', () => {
+    const { container } = render(<Icon style="regular" icon="star" />);
+    
+    expect(container.getElementsByClassName('fa-regular')).toBeDefined();
+    expect(container.getElementsByClassName('fa-star')).toBeDefined();
+  })
+})
