6961279b439054666c41ed7ef98e7dcab6eb012e
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-11 23:33:36 -0300

    feat: add and configure fontawesome and font Roboto

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { router } from './router.tsx'
 import { RouterProvider } from 'react-router-dom'
 
+import './assets/css/tokens.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
