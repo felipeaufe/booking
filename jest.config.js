3f851578480af453f832427fdad8c30f7340d255
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-11 23:34:40 -0300

    chore: configure jest import aliases

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,19 @@ export default {
   moduleNameMapper: {
 
       // if your using tsconfig.paths thers is no harm in telling jest
+    '@compositions/(.*)$': '<rootDir>/src/compositions/$1',
     '@components/(.*)$': '<rootDir>/src/components/$1',
+    '@elements/(.*)$': '<rootDir>/src/elements/$1',
+    '@services/(.*)$': '<rootDir>/src/services/$1',
+    '@config/(.*)$': '<rootDir>/src/config/$1',
+    '@pages/(.*)$': '<rootDir>/src/pages/$1',
     '@/(.*)$': '<rootDir>/src/$1',
       
       // mocking assests and styling
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>test-config/fileMock.ts',
     '^.+\\.(css|less|scss|sass)$': '<rootDir>test-config/styleMock.ts',
+    
     /* mock models and services folder */
     '(assets|models|services)': '<rootDir>test-config/fileMock.ts',
   },
@@ -19,4 +25,11 @@ export default {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePaths: ['<rootDir>'],
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      diagnostics: {
+        ignoreCodes: [151001],
+      }
+    }],
+  },
 };
--- test-config/setupTests.ts
+++ test-config/setupTests.ts
@@ -4,3 +4,12 @@
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
+
+global.window.document = document;
+global.console = {
+  ...console,
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
