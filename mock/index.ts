c61dd40cf87793bdb6d06d4b67a523e1c3b6b133
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-14 18:05:38 -0300

    chore: add mock files to bookings-service

--- mock/index.ts
+++ mock/index.ts
@@ -1,12 +1,27 @@
 import {v4 as uuidv4} from 'uuid';
 import Places from './data/places.json';
+import Bookings from './data/bookings.json';
 
 export default [
+  
+  /** !SECTION Places */
+
   {
     url: '/api/places',
     method: 'get',
     response: () => {
       return Places;
+    },
+  },
+
+
+  /** !SECTION Bookings */
+
+  {
+    url: '/api/bookings',
+    method: 'get',
+    response: () => {
+      return Bookings;
     },
   },
 
