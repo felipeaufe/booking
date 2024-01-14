71c52dc211a369bed251e06bfb331072f1f906c0
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-13 22:51:46 -0300

    feat: add booking-form and date utils

--- src/components/booking-form/booking-form.tsx
+++ src/components/booking-form/booking-form.tsx
@@ -1,14 +1,38 @@
-import { Input } from "@assets/styled/Input";
+import styled from "styled-components";
 import { Button } from "@assets/styled/button";
 import { GuestSelect } from "@components/guest-select/guest-select";
-import styled from "styled-components";
+import { ButtonDatePicker } from "@elements/button-date-picker/button-date-picker";
+import { useState } from "react";
+import { addDays } from "@utils/date";
 
 export function BookingForm () {
+
+  const [ checkIn, setCheckIn ] = useState<Date | null>(null);
+  const [ checkOut, setCheckOut ] = useState<Date | null>(null);
+
   return (
     <Container>
       <div>
-        <Input type="text" placeholder="Check-in"/>
-        <Input type="text" placeholder="Checkout"/>
+        <CheckIn
+          value={checkIn}
+          onChange={data => data && setCheckIn(data)}
+          includeDateIntervals={[
+            { start: addDays(new Date(), 5), end: addDays(new Date(), 30) },
+          ]}
+        >
+          Check-in{ checkIn ? `: ${checkIn?.toLocaleDateString()}` : ''}
+        </CheckIn>
+
+        <CheckOut
+          value={checkOut}
+          onChange={data => data && setCheckOut(data)}
+          disabled={!checkIn}
+          includeDateIntervals={[
+            { start: addDays(new Date(), 5), end: addDays(new Date(), 30) },
+          ]}
+        >
+          Checkout{ checkOut ? `: ${checkOut?.toLocaleDateString()}` : ''}
+        </CheckOut>
       </div>
       <GuestSelect />
       <Button variant="primary">Reserve</Button>
@@ -21,14 +45,23 @@ const Container = styled.div`
   flex-direction: column;
   gap: 12px;
 
-  & input, & button {
-   width: 100%;
-  }
 
   & > div {
     display: flex;
     flex-direction: row;
-    gap: 12px;
+  }
+
+  & .react-datepicker-wrapper {
+    display: flex;
+    width: 100%;
   }
 `;
 
+const CheckIn = styled(ButtonDatePicker)`
+  margin-right: calc(var(--spacing-12) / 2);
+  width: 100%;
+`
+const CheckOut = styled(ButtonDatePicker)`
+  margin-left: calc(var(--spacing-12) / 2);
+  width: 100%;
+`
---
+++ src/utils/date.ts
@@ -0,0 +1,27 @@
+/**
+ * Subtracts a specified number of days from a given date and returns the resulting date.
+ *
+ * @param {Date} date - The date to subtract from.
+ * @param {number} days - The number of days to subtract.
+ * @return {Date} The resulting date after subtracting the specified number of days.
+ */
+export function subDays(date: Date, days: number){
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() - days);
+
+  return newDate
+}
+
+/**
+ * Adds a specified number of days to a given date and returns the resulting date.
+ *
+ * @param {Date} date - The date to which days will be added.
+ * @param {number} days - The number of days to add to the date.
+ * @return {Date} The resulting date after adding the specified number of days.
+ */
+export function addDays(date: Date, days: number){
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  
+  return newDate
+}
