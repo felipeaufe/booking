d3340b36048aa14a2a8dd325b0f22a50ce50cb2a
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-14 17:45:59 -0300

    refactor: add a loading layer in the App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify';
-import { router } from './router.tsx'
-import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux';
-
-import { store } from '@state/store';
-import { Header } from '@compositions/header/header';
 
 import 'react-toastify/dist/ReactToastify.min.css';
 import './assets/css/tokens.css';
+import { App } from './app';
+import { store } from '@state/store';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Header />
-      <RouterProvider router={router}/>
+      <App />
     </Provider>
     <ToastContainer />
   </React.StrictMode>,
