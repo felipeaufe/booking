c18838f4453edf58e7e606e76a77785a314cdd6f
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-14 16:55:16 -0300

    style: add JSDoc on type WritableDraft

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,22 @@
+/**
+ * Represents a mutable draft object that can be used to modify an immutable state.
+ *
+ * @template T - The type of the original immutable state.
+ *
+ * @typedef {Object} WritableDraft
+ * @property {T} - The original immutable state object.
+ *
+ * @example
+ * // Create a readonly property
+ * interface User {
+ *   readonly name: string;
+ * }
+ * 
+ * Updating a property in the draft object
+ * function updateUser(user: WritableDraft<User>, name: string) {
+ *   user.name = name;
+ * }
+ */
 export type WritableDraft<T> = {
   -readonly [P in keyof T]: WritableDraft<T[P]>;
 };
