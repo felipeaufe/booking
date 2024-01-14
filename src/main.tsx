65976388b63cc5cff7a6a99cd73116b412dc3b91
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-14 16:09:59 -0300

    feat: configure toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify';
 import { router } from './router.tsx'
 import { RouterProvider } from 'react-router-dom'
-
-import './assets/css/tokens.css';
 import { Provider } from 'react-redux';
+
 import { store } from '@state/store';
 import { Header } from '@compositions/header/header';
+
+import 'react-toastify/dist/ReactToastify.min.css';
+import './assets/css/tokens.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,5 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Header />
       <RouterProvider router={router}/>
     </Provider>
+    <ToastContainer />
   </React.StrictMode>,
 )
