f0b79c2403333cf532990abed65fb41dccc0b7a9
Felipe Augusto <felipe.aufe@gmail.com>
2024-01-15 16:22:40 -0300

    refactor: call banner into the home page

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -1,15 +1,29 @@
+import "@test-config/mocks/react-router-dom";
 
-import '@test-config/mocks/swiper';
-import "@test-config/mocks/use-navigate";
-
-import Home from "./home";
 import { renderRedux } from '@test-config/test-utils/render';
 import { initialState } from '@state/places';
+import { act, screen } from "@testing-library/react";
+import { places } from "@test-config/mock-data/places";
+
+import Home from "./home";
 
 describe("Home", () => {
-  it('should render Home', () => {
-    const { container } = renderRedux(<Home />, { places: initialState });
+  it('should render Banner', () => {
+    renderRedux(<Home />, { places: initialState });
     
-    expect(container).toBeDefined();
+    expect(screen.getByText("Explore")).toBeDefined();
+    expect(screen.getByText("Beautiful")).toBeDefined();
+    expect(screen.getByText("World")).toBeDefined();
+    expect(screen.getByText("I Want To Travel")).toBeDefined();
+  })
+
+
+  it('should render places', async () => {
+    await act(async () => {
+      renderRedux(<Home />, { places: { ...initialState, data: places } });
+    })
+   
+    expect(screen.getByText("Explore Place")).toBeInTheDocument();
+    expect(screen.getAllByTestId("link")).toHaveLength(2);
   })
 });
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,10 @@
+import { Banner } from "@components/banner/banner";
 import { ExplorePlace } from "../../compositions/explore-place/explore-place";
 
 function Home() {
   return (
     <div> 
+      <Banner />
       <ExplorePlace />
     </div>
   )
